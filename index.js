e2f78f8670d03c7ca21475e3be8e55d34883a59d
Dan11r <dadondadon303@gmail.com>
2021-08-29 18:51:33 +0300

    исправление

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// repetition of lodash methods
 const chunk = (arr, num) => { // example  arg (['a', 'b', 'c', 'd', 'e'], 2)
     let result = [] // example returned [['a', 'b'], ['c', 'd'], ['e']]
     let part = []
@@ -17,12 +18,10 @@ const chunk = (arr, num) => { // example  arg (['a', 'b', 'c', 'd', 'e'], 2)
     return result
 }
 
-const compact = ((arr) => {
-    const result = []
+const compact = ((arr) => { // example  arg ([0, 1, false, 2, '', 3])
+    const result = []       // example returned [[1, 2, 3]]
     for (let i = 0; i < arr.length; i++) {
         !!arr[i] && result.push(arr[i])
     }
-
-    console.log(arr);
-    console.log(result);
+    return result
 })([0, 1, false, 2, '', 3])
